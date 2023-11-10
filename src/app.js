cff58fe567b2a7cf375fa18200ee1b8a3845eeb9
Miguel-A-VN <120611559+Miguel-A-VN@users.noreply.github.com>
2023-11-10 10:42:39 -0500

    agrege pdf del primer sprint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+//Requerimos expres y path
 const express = require('express');
 const path = require('path');
+
+//Ejecutamos expres y guardamos todas sus funciones en app
 const app = express();
+
+//Enlasamos la carpeta public para acceder a sus recursos
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 //call to server ↓
-
 app.listen(3030, () => {
     console.log('server online in the port 3000 => http://localhost:3030');
 });
